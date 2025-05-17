da04500eba8619f76c427a975b481f9f3c6ac121
Mohammad Hasibul Hasan <109550801+hasibulhimu49@users.noreply.github.com>
2025-05-17 09:31:09 +0600

    Update PalestineFlag.py

--- PalestineFlag.py
+++ PalestineFlag.py
@@ -1,57 +1,65 @@
 import turtle
-t = turtle.Turtle()
 
-turtle.setup(1000, 1000)
-t.penup()
-t.goto(-200, 120) 
-t.pendown()
+screen=turtle.Screen()
+screen.setup(1000,700)
 
+t=turtle.Turtle()
+turtle.speed(0)
 t.fillcolor("black")
 t.begin_fill()
-
-for i in range(2):
-    t.forward(400)
-    t.left(90)
-    t.forward(80)
-    t.left(90)
-
+t.penup()
+t.goto(-250,-100)
+t.pendown()
+t.forward(500)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(500)
+t.left(90)
+t.forward(100)
+t.left(90)
 t.end_fill()
 
 t.fillcolor("white")
 t.begin_fill()
-
-for i in range(2):
-    t.forward(400)
-    t.right(90)
-    t.forward(80)
-    t.right(90)
-
-t.end_fill()
-
 t.penup()
-t.goto(-200, -40) 
+t.goto(-250,0)
 t.pendown()
+t.forward(500)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(500)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.end_fill()
 
 t.fillcolor("green")
 t.begin_fill()
-
-for i in range(2):
-    t.forward(400)
-    t.left(90)
-    t.forward(80)
-    t.left(90)
-
+t.penup()
+t.goto(-250,100)
+t.pendown()
+t.forward(500)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(500)
+t.left(90)
+t.forward(100)
+t.left(90)
 t.end_fill()
 
 t.fillcolor("red")
 t.begin_fill()
-
+t.penup()
+t.goto(-250,-100)
+t.pendown()
 t.left(45)
-t.forward(170)
+t.forward(212)
 t.left(90)
-t.forward(170)
-t.left(135)
-t.forward(240)
+t.forward(212)
 t.end_fill()
+
 
 turtle.mainloop()
